d665219db12d8d07ceb7941ad457472940296234
andresshare <andresshare@gmail.com>
2019-09-26 09:40:58 -0500

    Implement Full Text Search

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  	scope :api do
+	scope :api do
     resources :books
     resources :authors
     resources :publishers
+
+    get '/search/:text', to: 'search#index'
   end
 
   root to: 'books#index'
